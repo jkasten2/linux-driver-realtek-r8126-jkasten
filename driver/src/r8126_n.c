b06bf0beab072b37a155ad6c7d8f64018b4c4d95
Josh Kasten <jkasten@gmail.com>
2025-02-02 02:40:48 +0000

    clean up dma_for_tx_buff_setup
    correct error handling and printk messages.

--- driver/src/r8126_n.c
+++ driver/src/r8126_n.c
@@ -4675,70 +4675,63 @@ static void rt8126_dma_for_tx_buff_unsetup(struct rtl8126_private *tp);
 // TODO: What is the max skb for skb_frag_size or skb->data?
 #define TX_PER_PACKET_BUFFER_SIZE 65536 // 64KiB
 
-static void rt8126_dma_for_tx_buff_setup(struct rtl8126_private *tp) {
-        printk(KERN_WARNING "rt8126 - Using ENABLE_TX_PAGE_REUSE - Unofficial Tweak1 by Josh Kasten - dma_for_tx_buff_setup");
-
-        printk(KERN_WARNING "rt8126 - tp->tx_ring[0].num_tx_desc: %u", tp->tx_ring[0].num_tx_desc);
+static int rt8126_dma_for_tx_buff_setup(struct rtl8126_private *tp) {
+        printk(KERN_INFO "rt8126 - Using ENABLE_TX_PAGE_REUSE - Unofficial Tweak1 by Josh Kasten - dma_for_tx_buff_setup");
 
         for(int ring_num = 0; ring_num < tp->num_tx_rings; ring_num++) {
                 int tx_ring_size = tp->tx_ring[ring_num].num_tx_desc;
-        printk(KERN_WARNING "rt8126 - sizeof(void*) * tx_ring_size: %u", sizeof(void*) * tx_ring_size);
+                printk(KERN_INFO "rt8126 - sizeof(void*) * tx_ring_size: %u", sizeof(void*) * tx_ring_size);
 
                 tp->tx_ring[ring_num].tx_kmem_buffers = kzalloc(sizeof(void*) * tx_ring_size, GFP_KERNEL);
                 tp->tx_ring[ring_num].tx_dma_buffers = kzalloc(sizeof(void*) * tx_ring_size, GFP_KERNEL);
 
-        bool got_err = false;
-        for(unsigned int i = 0; i < tx_ring_size; i++) {
+                for(int i = 0; i < tx_ring_size; i++) {
                         tp->tx_ring[ring_num].tx_kmem_buffers[i] = kmalloc(TX_PER_PACKET_BUFFER_SIZE, GFP_KERNEL);
                         if (tp->tx_ring[ring_num].tx_kmem_buffers[i] == 0) {
-                        printk(KERN_WARNING "rt8126 - kmalloc falled!");
-                        got_err = true;
+                                printk(KERN_ERR "rt8126 - kmalloc falled!");
+                                return -1;
                         break;
                 } else {
                                 dma_addr_t dma_addr = dma_map_single(tp_to_dev(tp), tp->tx_ring[ring_num].tx_kmem_buffers[i], TX_PER_PACKET_BUFFER_SIZE, DMA_TO_DEVICE);
                                 tp->tx_ring[ring_num].tx_dma_buffers[i] = dma_addr;
 
                         if (dma_addr == 0) {
-                                printk(KERN_ERR "dma_addr is 0");
+                                        printk(KERN_ERR "rt8126 - dma_addr is 0");
                                 int dma_error = dma_mapping_error(tp_to_dev(tp), dma_addr);
                                 printk(KERN_ERR "rt8126 - dma_error: %u", dma_error);
-                                got_err = true;
+                                        return -1;
                                 break;
                         } else {
                                 int dma_error = dma_mapping_error(tp_to_dev(tp), dma_addr);
                                 if (dma_error != 0) {
                                         printk(KERN_ERR "rt8126 - dma_error: %u", dma_error);
-                                        got_err = true;
+                                                return -1;
                                         break;
                                 }
                         }
                 }
         }
+        }
 
-        if (got_err) {
-                // TODO: Test code path
-                rt8126_dma_for_tx_buff_unsetup(tp);
-        }
-        else {
-                printk(KERN_WARNING "rt8126 - rt8126_dma_for_tx_buff_setup - success");
-                }
-        }
+        printk(KERN_INFO "rt8126 - rt8126_dma_for_tx_buff_setup - success");
+        return 1;
 }
 
 static void rt8126_dma_for_tx_buff_unsetup(struct rtl8126_private *tp) {
         for(int ring_num = 0; ring_num < tp->num_tx_rings; ring_num++) {
-                for(unsigned int i = 0; i < tp->tx_ring[ring_num].num_tx_desc; i++) {
+                for(int i = 0; i < tp->tx_ring[ring_num].num_tx_desc; i++) {
                         dma_addr_t cur_dma = tp->tx_ring[ring_num].tx_dma_buffers[i];
-                if (cur_dma != 0) {
+                        if (cur_dma != 0)
                         dma_unmap_single(tp_to_dev(tp), cur_dma, TX_PER_PACKET_BUFFER_SIZE, DMA_TO_DEVICE);
-                }
-                        kfree(tp->tx_ring[ring_num].tx_kmem_buffers[i]);
+                        void* kmem = tp->tx_ring[ring_num].tx_kmem_buffers[i];
+                        if (kmem != 0)
+                                kfree(kmem);
         }
                 kfree(tp->tx_ring[ring_num].tx_kmem_buffers);
                 kfree(tp->tx_ring[ring_num].tx_dma_buffers);
         }
 
-        printk(KERN_WARNING "rt8126 - rt8126_dma_for_tx_buff_unsetup - success");
+        printk(KERN_INFO "rt8126 - rt8126_dma_for_tx_buff_unsetup - success");
 }
 // End
 
@@ -15120,9 +15113,9 @@ int rtl8126_open(struct net_device *dev)
         if (retval < 0)
                 goto err_free_all_allocated_mem;
 
-        // Kasten - TODO: Fail or fall back if there is an error
-        // TODO2: Works here, but is this the best spot?
-        rt8126_dma_for_tx_buff_setup(tp);
+        retval = rt8126_dma_for_tx_buff_setup(tp);
+        if (retval < 0)
+                 goto err_free_all_allocated_mem;
 
         retval = rtl8126_alloc_irq(tp);
         if (retval < 0)
